c7dc8d901e262ab3b36c37f5f980a742126d30f9
Inês Moreira <simoreira@ua.pt>
2018-11-08 18:16:54 +0000

    Add zoom and search

--- AppGui/AppGui/MainWindow.xaml.cs
+++ AppGui/AppGui/MainWindow.xaml.cs
@@ -1,31 +1,105 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using System.Xml.Linq;
 using mmisharp;
 using Newtonsoft.Json;
-using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
 namespace AppGui
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
         private MmiCommunication mmiC;
+        private double ZoomValue = 100;
+        private double ZoomIncrement = 10;
+        private IWebDriver driver;
+        private IJavaScriptExecutor js;
         public MainWindow()
         {
+
             InitializeComponent();
+            InitializeFirefox();
 
-
-            mmiC = new MmiCommunication("localhost",8000, "User1", "GUI");
+            mmiC = new MmiCommunication("localhost", 8000, "User1", "GUI");
             mmiC.Message += MmiC_Message;
             mmiC.Start();
 
-            IWebDriver driver = new FirefoxDriver();
-            driver = new FirefoxDriver(Environment.CurrentDirectory);
-            driver.Url = "http://www.demoqa.com";
+        }
+
+        private void InitializeFirefox()
+        {
+            driver = new ChromeDriver(Environment.CurrentDirectory);
+            driver.Manage().Window.Maximize();
+            driver.Url = "http://www.github.com";
+            Search();
+
+        }
+
+        private void ZoomIn()
+        {
+            ZoomValue += ZoomIncrement;
+            Zoom(ZoomValue);
+
+
+        }
+
+        private void ZoomOut()
+        {
+            ZoomValue -= ZoomIncrement;
+            Zoom(ZoomValue);
+        }
+
+        private void Zoom(double level)
+        {
+            js = (IJavaScriptExecutor)driver;
+            Console.WriteLine(level.ToString().Replace(',', '.'));
+            js.ExecuteScript(string.Format("document.body.style.zoom='{0}%'", level.ToString().Replace(',', '.')));
+        }
+
+        private void Search()
+        {
+            js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("window.open('http://www.google.pt', 'newtab');");
+            driver.SwitchTo().Window("newtab");
+            IWebElement search = driver.FindElement(By.Name("q"));
+            search.SendKeys("sinónimos de bolacha");
+            Actions actions = new Actions(driver);
+            IWebElement feelingLuckyButton = null;
+            for (int i = 2; i < 12; i++)
+            {
+
+                try
+                {
+                    feelingLuckyButton = driver.FindElement(By.XPath("//*[@id='sbtc']/div[2]/div[2]/div[1]/div/ul/li[" + i + "]/div/span[2]/span/input"));
+                }
+                catch (Exception)
+                {
+                }
+                if (feelingLuckyButton == null && i == 11)
+                {
+                    feelingLuckyButton = driver.FindElement(By.XPath("//*[@id='tsf']/div[2]/div[3]/center/input[2]"));
+                }
+                else if (feelingLuckyButton == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            actions.MoveToElement(feelingLuckyButton).Click().Perform();
 
         }
 
@@ -35,15 +109,18 @@ namespace AppGui
             var doc = XDocument.Parse(e.Message);
             var com = doc.Descendants("command").FirstOrDefault().Value;
             dynamic json = JsonConvert.DeserializeObject(com);
-            /*
+
             Shape _s = null;
             switch ((string)json.recognized[0].ToString())
             {
-                case "SQUARE": _s = rectangle;
+                case "SQUARE":
+                    _s = rectangle;
                     break;
-                case "CIRCLE": _s = circle;
+                case "CIRCLE":
+                    _s = circle;
                     break;
-                case "TRIANGLE": _s = triangle;
+                case "TRIANGLE":
+                    _s = triangle;
                     break;
             }
 
@@ -68,8 +145,8 @@ namespace AppGui
                         break;
                 }
             });
-            */
-            
+
+
 
 
         }
